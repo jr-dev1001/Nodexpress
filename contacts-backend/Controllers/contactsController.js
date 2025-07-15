5ae78f69d0b212a1a830217414dcfcfc81d68b38
jr-dev1001 <vamsisurakarapu@gmail.com>
2025-07-15 16:32:20 +0530

    async handler to handle promises from mongo

--- contacts-backend/Controllers/contactsController.js
+++ contacts-backend/Controllers/contactsController.js
@@ -1,7 +1,8 @@
+
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access Public
-const getContacts = (req, res) => {
+const getContacts = async (req, res) => {
     res.status(200).json({ message: 'get all contacts' });
 }
 
@@ -9,15 +10,20 @@ const getContacts = (req, res) => {
 //@desc Create a new contact
 //@route POST /api/contacts 
 //@access Public
-const createContact = (req, res) => {
+const createContact = async (req, res) => {
     console.log("request body is ",req.body);
+    const {name, email, phone} = req.body;
+    if(!name || !email || !phone) {
+        res.status(400);
+        throw new Error('Please add all fields');
+    }
     res.status(200).json({ message: 'created contact' });
 }
 
 //@desc Get contact by ID
 //@route GET /api/contacts/:id
 //@access Public
-const getContact = (req, res) => {
+const getContact = async (req, res) => {
     res.status(200).json({ message: `get contact id ${req.params.id}` });
 }
 
@@ -25,14 +31,14 @@ const getContact = (req, res) => {
 //@desc Update contact by ID
 //@route PUT /api/contacts/:id
 //@access Public
-const updateContact = (req, res) => {
+const updateContact = async (req, res) => {
     res.status(200).json({ message: `updated contact for ${req.params.id}` });
 }
 
 //@desc Delete contact by ID
 //@route DELETE /api/contacts/:id
 //@access Public
-const deleteContact = (req, res) => {
+const deleteContact = async (req, res) => {
     res.status(200).json({ message: `deleted contact for ${req.params.id}` });
 }
 
